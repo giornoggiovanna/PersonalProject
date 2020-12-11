6acde8b1cc04e63963003c1bf63d329fb3377bee
giornogiovanna <hayden.zsm@outlook.com>
2020-12-11 18:34:17 +0100

    Push 1.10 Added weapons system, some organizing

---
+++ projectTests/MovementAlpha2/Assets/Scripts/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public Rigidbody2D myRB;
+    public float damage;
+    public Animator myAnim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       myAnim = GetComponent<Animator>(); 
+       myRB.velocity = transform.right * speed; 
+    }
+
+    private void OnTriggerEnter2D (Collider2D enemy) 
+    {
+        print(enemy.name);
+        Destroy(gameObject);
+        myAnim.SetBool ("isFired", true);
+
+        if (enemy.tag == "Alien")
+        {
+            AlienController alienController = enemy.gameObject.GetComponent<AlienController> ();
+            alienController.gunAlienTakeDamage(damage);
+
+        }
+
+        if (enemy.tag == "Asteroid")
+        {
+            asteroidHealthController asteroidHealth = enemy.gameObject.GetComponent<asteroidHealthController> ();
+            asteroidHealth.asteroidTakeDamage(damage);
+
+        }
+    }
+
+    
+    // Update is called once per frame
+  
+}
